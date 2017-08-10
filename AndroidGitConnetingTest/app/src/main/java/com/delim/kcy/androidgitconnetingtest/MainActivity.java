5c5078c2dfa60fbc60cd4d051d3d5991d9e27dbd
y2kim <kchy0806@naver.com>
2017-08-10 16:03:26 +0900

    활용
    연습 100% 완료

--- AndroidGitConnetingTest/app/src/main/java/com/delim/kcy/androidgitconnetingtest/MainActivity.java
+++ AndroidGitConnetingTest/app/src/main/java/com/delim/kcy/androidgitconnetingtest/MainActivity.java
@@ -2,14 +2,19 @@ package com.delim.kcy.androidgitconnetingtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import kr.go.seoul.culturalevents.CulturalEventButtonTypeB;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String key = "5a6c4f63676b636836384858566346";
+    private CulturalEventButtonTypeB typeB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        typeB = (CulturalEventButtonTypeB) findViewById(R.id.type_b);
+        typeB.setOpenAPIKey(key);
     }
 }
